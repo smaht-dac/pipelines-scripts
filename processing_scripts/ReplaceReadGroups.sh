2d522abb05b24380c36f70d2c8e3ea48501bbd21
Michele Berselli <berselli.michele@gmail.com>
2025-07-29 13:39:35 -0400

    added cram support

--- processing_scripts/ReplaceReadGroups.sh
+++ processing_scripts/ReplaceReadGroups.sh
@@ -1,83 +1,171 @@
 #!/usr/bin/env bash
 
 # *******************************************
-# Replace SAMPLE and LIBRARY or
-# only SAMPLE across read groups in BAM input file.
-# Index the ouput file.
+# Replace sample name (and optionally library) in all @RG lines
+# of a BAM or CRAM file. Reheader and index the output.
 # *******************************************
 
 ## Usage
-USAGE="Usage: ReplaceReadGroups.sh -i <input_file_bam> -s <sample_name> [-l <library>] [-o output_prefix]"
+usage() {
+  cat << EOF
+Usage: ReplaceReadGroups.sh [options]
+
+Required arguments:
+  -i | --input <input_file>        Input BAM or CRAM file
+  -s | --sample <sample_name>      Sample name to replace in all @RG entries
+
+Optional arguments:
+  -l | --library <library>         Library name to replace in all @RG entries
+  -o | --output <output_prefix>    Output prefix (default: 'output')
+  -r | --reference <fasta>         Reference FASTA (required for CRAM input)
+  --in_place                       Overwrite original CRAM file in place (CRAM only)
+  -h | --help                      Show this help message
+EOF
+  exit 1
+}
 
 ## Default args
 output_prefix="output"
 nt=$(nproc)
+in_place=false
+is_cram=false
 
 ## Functions
-check_args()
-{
-  arg_names=($@)
-  for arg_name in ${arg_names[@]}; do
-      [ -z ${!arg_name} ] && \
-      echo "Missing Argument <${arg_name}>" 1>&2 && \
-      echo $USAGE 1>&2 && \
-      exit 1
+err() { echo "[ERROR] $*" >&2; }
+
+check_args() {
+  arg_names=("$@")
+  for arg_name in "${arg_names[@]}"; do
+    if [ -z "${!arg_name}" ]; then
+      err "Missing argument: <$arg_name>"
+      usage
+    fi
   done
-  return 0
 }
 
-## Bash command line definition
-while getopts 'i:s:l:o:h' opt; do
-  case $opt in
-    # Required arguments
-    i) input_file_bam=${OPTARG} ;;
-    s) sample_name=${OPTARG} ;;
-    # Optional arguments
-    l) library=${OPTARG} ;;
-    o) output_prefix=${OPTARG} ;;
-    ?|h)
-      echo $USAGE 1>&2
-      exit 1
+## Parse arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -i|--input)
+      input_file="$2"
+      shift 2
+      ;;
+    -s|--sample)
+      sample_name="$2"
+      shift 2
+      ;;
+    -l|--library)
+      library="$2"
+      shift 2
+      ;;
+    -o|--output)
+      output_prefix="$2"
+      shift 2
+      ;;
+    -r|--reference)
+      reference_fasta="$2"
+      shift 2
+      ;;
+    --in_place)
+      in_place=true
+      shift
+      ;;
+    -h|--help)
+      usage
+      ;;
+    *)
+      err "Unknown argument: $1"
+      usage
       ;;
   esac
 done
-shift $(($OPTIND -1))
 
-## Check arguments
-check_args input_file_bam sample_name
+## Check required args
+check_args input_file sample_name
 
-# Update @RG
-if [ ! -z ${library} ]; then
-  samtools view --no-PG -H $input_file_bam | \
-    sed -e "/^@RG/ s/SM:[^\t]*/SM:${sample_name}/" -e "/^@RG/ s/LB:[^\t]*/LB:${sample_name}.${library}/" | \
-    samtools reheader --no-PG - $input_file_bam > ${output_prefix}.bam || exit 1
-else
-  samtools view --no-PG -H $input_file_bam | \
-    sed -e "/^@RG/ s/SM:[^\t]*/SM:${sample_name}/" | \
-    samtools reheader --no-PG - $input_file_bam > ${output_prefix}.bam || exit 1
+## Detect file extension
+input_ext=$(basename "$input_file" | awk -F. '{print tolower($NF)}')
+[[ "$input_ext" == "cram" ]] && is_cram=true
+
+## Check CRAM-specific args
+if $is_cram; then
+  check_args reference_fasta
+  if $in_place; then
+    echo "[WARNING] Overwriting original CRAM file: $input_file"
+  fi
+elif $in_place; then
+  err "--in_place is only supported for CRAM input files"
+  usage
 fi
 
-# Index
-samtools index -@ $nt ${output_prefix}.bam || exit 1
+## Modify read group headers
+if ! $is_cram; then
+  # BAM case
+  if [ -n "$library" ]; then
+    samtools view --no-PG -H "$input_file" | \
+      sed -e "/^@RG/ s/SM:[^\t]*/SM:${sample_name}/" \
+          -e "/^@RG/ s/LB:[^\t]*/LB:${sample_name}.${library}/" | \
+      samtools reheader --no-PG - "$input_file" > "${output_prefix}.bam" || exit 1
+  else
+    samtools view --no-PG -H "$input_file" | \
+      sed -e "/^@RG/ s/SM:[^\t]*/SM:${sample_name}/" | \
+      samtools reheader --no-PG - "$input_file" > "${output_prefix}.bam" || exit 1
+  fi
+else
+  # CRAM case
+  trap 'rm -f tmp_header' EXIT
 
-# Check BAM integrity
-py_script="
+  # Create temporary header with modified read groups
+  if [ -n "$library" ]; then
+    samtools view --no-PG -H -T "$reference_fasta" "$input_file" | \
+      sed -e "/^@RG/ s/SM:[^\t]*/SM:${sample_name}/" \
+          -e "/^@RG/ s/LB:[^\t]*/LB:${sample_name}.${library}/" > tmp_header || exit 1
+  else
+    samtools view --no-PG -H -T "$reference_fasta" "$input_file" | \
+      sed -e "/^@RG/ s/SM:[^\t]*/SM:${sample_name}/" > tmp_header || exit 1
+  fi
+
+  # Apply header to CRAM
+  samtools reheader --no-PG -i tmp_header "$input_file" || exit 1
+
+  # If not in-place, convert to BAM
+  if ! $in_place; then
+    samtools view -@ "$nt" --no-PG -h --bam -T "$reference_fasta" -o "${output_prefix}.bam" "$input_file" || exit 1
+  fi
+fi
+
+# Index output BAM or CRAM
+if [[ -f "${output_prefix}.bam" ]]; then
+  samtools index -@ "$nt" "${output_prefix}.bam" || exit 1
+
+  # Check BAM EOF
+  py_script="
 import sys, os
-
 def check_EOF(filename):
     EOF_hex = b'\x1f\x8b\x08\x04\x00\x00\x00\x00\x00\xff\x06\x00\x42\x43\x02\x00\x1b\x00\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00'
     size = os.path.getsize(filename)
-    fb = open(filename, 'rb')
-    fb.seek(size - 28)
-    EOF = fb.read(28)
-    fb.close()
-    if EOF != EOF_hex:
-        sys.stderr.write('EOF is missing\n')
-        sys.exit(1)
-    else:
-        sys.stderr.write('EOF is present\n')
-
+    with open(filename, 'rb') as f:
+        f.seek(size - 28)
+        if f.read(28) != EOF_hex:
+            sys.stderr.write('EOF is missing\\n')
+            sys.exit(1)
+        else:
+            sys.stderr.write('EOF is present\\n')
 check_EOF('${output_prefix}.bam')
 "
+  python -c "$py_script" || exit 1
+else
+  mv "${input_file}" "${output_prefix}.cram" || exit 1
+  samtools index -@ "$nt" "${output_prefix}.cram" || exit 1
+fi
 
-python -c "$py_script" || exit 1
+# Print output files
+echo "[INFO] Finished. Output:"
+[[ -f "${output_prefix}.bam" ]] && {
+  echo "  - ${output_prefix}.bam"
+  echo "  - ${output_prefix}.bam.bai"
+}
+[[ -f "${output_prefix}.cram" ]] && {
+  echo "  - ${output_prefix}.cram"
+  echo "  - ${output_prefix}.cram.crai"
+}
